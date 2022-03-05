19761e87292565d147b8cec7aeb7b741dc845160
Noah Schiff <nps39@cornell.edu>
2022-03-04 20:48:25 -0500

    Add empty implementation to model.ml

---
+++ src/model.ml
@@ -0,0 +1,10 @@
+type cell = { nation : string }
+type world = cell array array
+
+let get_cell world x y = failwith "TODO"
+let get_size world = (0, 0)
+let get_nation cell = failwith "TODO"
+let get_coordinate cell = (0, 0)
+let simulate world = world
+let clear_cell world x y = world
+let inject_cell world x y nation = failwith "TODO"
