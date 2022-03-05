aeaeafbb6e9cf58fbc72f520be58dcba9cd3b420
Rigel Wafford <rcw248@cornell.edu>
2022-03-04 21:32:32 -0500

    Added a view test method for drafting a view

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,22 @@
-let () = print_endline "Hello, World!"
+open Bogue
+module W = Widget
+module L = Layout
+
+let test_layout =
+  let input = W.text_input ~max_size:200 ~prompt:"Enter your name" () in
+  let label = W.label ~size:40 "Hello!" in
+  let layout =
+    L.tower [ L.resident ~w:400 input; L.resident ~w:400 ~h:200 label ]
+  in
+  let action ti l _ =
+    let text = W.get_text ti in
+    W.set_text l ("Hello " ^ text ^ "!")
+  in
+  let c =
+    W.connect input label action Trigger.[ text_input; key_down ]
+  in
+
+  let board = Bogue.make [ c ] [ layout ] in
+  Bogue.run board
+
+let main () = test_layout
--- src/view.ml
+++ src/view.ml
@@ -1,23 +1,24 @@
+open Bogue
+module W = Widget
+module L = Layout
+
 type t = {
   render_count : int;
   zoom : int;
 }
 
 type event = int
-
 type event_handler = event -> unit
 
 type interaction_manager = {
-    getcell : event_handler;
-    setcell : event_handler;
-    play : event_handler;
-    pause : event_handler;
-    save : event_handler;
-    load : event_handler
+  getcell : event_handler;
+  setcell : event_handler;
+  play : event_handler;
+  pause : event_handler;
+  save : event_handler;
+  load : event_handler;
 }
 
 let init interaction_manager = failwith "Unimplemented"
-
 let render view = failwith "Unimplemented"
-
 let set_zoom view dx dy zoom = failwith "Unimplemented"
