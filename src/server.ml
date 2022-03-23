406850f23db99577d93fe73a5e46f42958fd90cb
Ari Mirsky <amm487@cornell.edu>
2022-03-23 01:53:15 -0400

    Mostly broken slightly working server
    I know that Noah especially will be angry that I committed broken code to master, but I really am just lost at making this server. The issues are 1. I can't find a way to read in an html document/otherwise display an html document on localhost and 2. Running localhost on the base page gives 'Internal server error', but there is no evidence of this error anywhere else on the internet :). A third error that might come up if you mess with stuff is some vague unix fault, and that happens when something has already claimed a port that it's trying to run on. In that case I found that the easiest thing is to just restart your computer because some processes are hard to kill. Lastly, here are some things that I found helpful (but considering I didn't get it working, idk how helpful they were): https://github.com/rgrinberg/opium/tree/master/example, https://github.com/EduardoRFS/youtube-channel/tree/master/05-making-crud-in-ocaml/code, https://shonfeder.gitlab.io/ocaml_webapp/, and the official opium documentation https://rgrinberg.github.io/opium/opium/index.html. I wish you luck.

--- src/server.ml
+++ src/server.ml
@@ -9,17 +9,27 @@ let get req =
 
 let post (push : t) req =
   let json = req |> Request.to_json_exn in
-  Lwt.bind json (fun x -> Some x |> push |> Lwt.return);
+  ignore @@ Lwt.bind json (fun x -> Some x |> push |> Lwt.return);
   (fun _json ->
     Lwt.return
       (Response.make ~body:(Body.of_string "Received response") ()))
     json
+
+let get_html _req =
+  let my_page =
+    let open Tyxml.Html in
+    html
+      (head (title (txt "Title")) [])
+      (body [ h1 [ txt "Hello World!" ] ])
+  in
+  my_page |> Response.of_html |> Lwt.return
 
 let init (push : Yojson.Safe.t option -> unit) : t =
   begin
     match
       App.empty |> App.port 3000 |> App.cmd_name "clans"
       |> App.get "/" get
+      |> App.get "/index.html" get_html
       |> App.post "/" (post push)
       |> App.run_command'
     with
