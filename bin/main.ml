8499a23bdc2f3b5af26fbe08e6ccc5b15802ceb2
Ari Mirsky <amm487@cornell.edu>
2022-03-25 23:51:49 -0400

    Implimented main

--- bin/main.ml
+++ bin/main.ml
@@ -1,3 +1,12 @@
 open Clans
 
-let _ = Server.init (fun x -> ())
+let s, p = Lwt_stream.create ()
+
+let rec recieve _ =
+  Lwt.bind
+    (Lwt.bind (Lwt_stream.last_new s) Controller.send_something)
+    (fun x -> recieve x)
+
+let _ =
+  Controller.init p;
+  recieve ()
