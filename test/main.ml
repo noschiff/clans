0c61d5c5f28ab45a67c05478ed1ad7a88e0140bd
UnsignedByte <edlam1889@gmail.com>
2022-03-28 17:44:51 -0700

    init test suite

--- test/main.ml
+++ test/main.ml
@@ -1,48 +1,21 @@
-open Opium
+(*
+* @Author: UnsignedByte
+* @Date:   2022-03-28 17:40:34
+* @Last Modified by:   UnsignedByte
+* @Last Modified time: 2022-03-28 17:43:41
+*)
+open OUnit2
+open Clans
 
-module Person = struct
-  type t = {
-    name : string;
-    age : int;
-  }
+let data_dir = "data/test/"
 
-  let yojson_of_t t =
-    `Assoc [ ("name", `String t.name); ("age", `Int t.age) ]
+let controller_tests = [
+  "Empty 1x1 world test" >:: (fun _ -> 
+    Controller.save_to_file (data_dir ^ "empty1x1.json") (Model.new_world 1 1));
+]
 
-  let t_of_yojson yojson =
-    match yojson with
-    | `Assoc [ ("name", `String name); ("age", `Int age) ] ->
-        { name; age }
-    | _ -> failwith "invalid person json"
-end
+let suite =
+  "test suite for Clans"
+  >::: List.flatten [ controller_tests; ]
 
-let print_person_handler req =
-  let name = Router.param req "name" in
-  let age = Router.param req "age" |> int_of_string in
-  let person = { Person.name; age } |> Person.yojson_of_t in
-  Lwt.return (Response.of_json person)
-
-let update_person_handler req =
-  let open Lwt.Syntax in
-  let+ json = Request.to_json_exn req in
-  let person = Person.t_of_yojson json in
-  Logs.info (fun m -> m "Received person: %s" person.Person.name);
-  Response.of_json (`Assoc [ ("message", `String "Person saved") ])
-
-let streaming_handler req =
-  let length = Body.length req.Request.body in
-  let content = Body.to_stream req.Request.body in
-  let body = Lwt_stream.map String.uppercase_ascii content in
-  Response.make ~body:(Body.of_stream ?length body) () |> Lwt.return
-
-let print_param_handler req =
-  Printf.sprintf "Hello, %s\n" (Router.param req "name")
-  |> Response.of_plain_text |> Lwt.return
-
-let _ =
-  App.empty
-  |> App.post "/hello/stream" streaming_handler
-  |> App.get "/hello/:name" print_param_handler
-  |> App.get "/person/:name/:age" print_person_handler
-  |> App.patch "/person" update_person_handler
-  |> App.run_command
+let _ = run_test_tt_main suite
