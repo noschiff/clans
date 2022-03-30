592f40be300b9ad09bea700e6dbb7ace948d285a
Ari Mirsky <amm487@cornell.edu>
2022-03-30 18:08:19 -0400

    Added debug

--- src/server.ml
+++ src/server.ml
@@ -2,13 +2,17 @@ open Opium
 
 type t = (string * Yojson.Safe.t * Yojson.Safe.t Lwt.u) option -> unit
 
+let debug = false
+
 (** Posts cell to stream to be dealt with later, and returns a message
     with whether the json was successfully parsed back to the client. *)
 let post name (push : t) req =
   Printf.printf "Recieve post %s\n" name;
   let content = Body.to_string req.Request.body in
   let respond x =
-    (* Printf.printf "Responding with %s\n" (Yojson.Safe.show x); *)
+    if debug then
+      Printf.printf "Responding with %s\n" (Yojson.Safe.show x)
+    else ();
     x |> Response.of_json
     |> Response.add_header ("Access-Control-Allow-Origin", "*")
     |> Lwt.return
@@ -18,9 +22,7 @@ let post name (push : t) req =
     Lwt.bind content (fun x ->
         x |> Yojson.Safe.from_string
         |> (fun x -> Some (name, x, res))
-        |> push
-        |> Lwt.return
-      )
+        |> push |> Lwt.return)
   with
   | exception _ -> respond (`Assoc [ ("status", `String "Failed") ])
   | _ -> Lwt.bind ret respond
@@ -28,9 +30,11 @@ let post name (push : t) req =
 let init (push : t) =
   App.empty |> App.port 3000
   |> (fun x ->
-     [ "update_cell"; "step" ]
-     |> List.fold_left
-        (fun acc s -> App.post ("/" ^ s) (post s push) acc)
-        x
-   )
+       [ "update_cell"; "step" ]
+       |> List.fold_left
+            (fun acc s -> App.post ("/" ^ s) (post s push) acc)
+            x)
+  |> (fun x ->
+       if debug then print_endline "Server.init completed" else ();
+       x)
   |> App.run_command
